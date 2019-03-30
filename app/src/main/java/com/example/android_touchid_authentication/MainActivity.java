5d291201f0b0cad7ef5bb21e18380332b409094c
kg0r0 <kg0r0@yahoo.co.jp>
2019-03-30 21:46:08 +0900

    Add verify process

--- app/src/main/java/com/example/android_touchid_authentication/MainActivity.java
+++ app/src/main/java/com/example/android_touchid_authentication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_touchid_authentication;
 import android.app.KeyguardManager;
 import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
+import android.hardware.fingerprint.FingerprintManager.CryptoObject;
 import android.os.Handler;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyPermanentlyInvalidatedException;
@@ -10,11 +11,14 @@ import android.security.keystore.KeyProperties;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPairGenerator;
@@ -22,7 +26,10 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Signature;
+import java.security.SignatureException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.security.spec.ECGenParameterSpec;
@@ -38,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY_NAME = "my_key";
     private static final String KEY_PROVIDER = "AndroidKeyStore";
 
+    private TextView confirmationMessage;
+
     private void init () {
         mKeyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
         mFingerprintManager = getApplicationContext().getSystemService(FingerprintManager.class);
@@ -46,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                     KeyProperties.KEY_ALGORITHM_EC, KEY_PROVIDER);
             mKeyStore = KeyStore.getInstance("AndroidKeyStore");
             mSignature = Signature.getInstance("SHA256withECDSA");
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -57,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         Button authenticateButton = (Button) findViewById(R.id.authenticate_button);
+        confirmationMessage = (TextView) findViewById(R.id.confirmation_message);
+
         if (!mKeyguardManager.isKeyguardSecure()) {
             // Show a message that the user hasn't set up a lock screen.
             Toast.makeText(this,"Secure lock screen hasn't set up.\n"
@@ -77,29 +89,62 @@ public class MainActivity extends AppCompatActivity {
 
         createKeyPair();
         authenticateButton.setOnClickListener(v -> {
-            if(initSignature()) {
+            CryptoObject mCryptoObject = initSignature();
+            if(mCryptoObject != null) {
                 DialogFragment dialog = new FingerPrintDiaglogFragment();
                 dialog.show(getSupportFragmentManager(), "dialog");
 
-                mFingerprintManager.authenticate(null, null, 0, new FingerprintManager.AuthenticationCallback() {
+                mFingerprintManager.authenticate(mCryptoObject, null, 0, new FingerprintManager.AuthenticationCallback() {
                     @Override
                     public void onAuthenticationError(int errorCode, CharSequence errString) {
                         Toast.makeText(MainActivity.this, "onAuthenticationError: " + errString, Toast.LENGTH_SHORT).show();
+                        confirmationMessage.setText("Failed");
+                        dialog.dismiss();
                     }
 
                     @Override
                     public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
                         Toast.makeText(MainActivity.this, "onAuthenticationHelp: " + helpString, Toast.LENGTH_SHORT).show();
+                        confirmationMessage.setText("Failed");
+                        dialog.dismiss();
                     }
 
                     @Override
                     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
                         Toast.makeText(MainActivity.this, "onAuthenticationSucceeded", Toast.LENGTH_SHORT).show();
+                        Signature resultSignature = result.getCryptoObject().getSignature();
+                        try {
+                            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+                            secureRandom.nextBytes(new byte[32]);
+                            byte[] secureRandomBytes = ByteBuffer.allocate(10).putLong(secureRandom.nextLong()).array();
+                            resultSignature.update(secureRandomBytes);
+                            byte[] sigBytes = resultSignature.sign();
+                            PublicKey publicKey =
+                                    mKeyStore.getCertificate(MainActivity.KEY_NAME).getPublicKey();
+                            Signature verificationFunction = Signature.getInstance("SHA256withECDSA");
+                            verificationFunction.initVerify(publicKey);
+                            verificationFunction.update(secureRandomBytes);
+
+
+                            // TODO 署名検証（通信）
+                            if (verificationFunction.verify(sigBytes)) {
+                                confirmationMessage.setText("Success");
+                            } else {
+                                confirmationMessage.setText("Failed");
+                            }
+                            confirmationMessage.setVisibility(View.VISIBLE);
+                            dialog.dismiss();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+
                     }
 
                     @Override
                     public void onAuthenticationFailed() {
                         Toast.makeText(MainActivity.this, "onAuthenticationFailed", Toast.LENGTH_SHORT).show();
+                        confirmationMessage.setText("Failed");
+                        dialog.dismiss();
                     }
                 }, new Handler());
 
@@ -113,14 +158,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private boolean initSignature() {
+    private CryptoObject initSignature() {
         try {
             mKeyStore.load(null);
             PrivateKey key = (PrivateKey) mKeyStore.getKey(KEY_NAME, null);
             mSignature.initSign(key);
-            return true;
+            CryptoObject cryptObject = new CryptoObject(mSignature);
+            return cryptObject;
         } catch (KeyPermanentlyInvalidatedException e) {
-            return false;
+            throw new RuntimeException("Failed to init Cipher", e);
         } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException
                 | NoSuchAlgorithmException | InvalidKeyException e) {
             throw new RuntimeException("Failed to init Cipher", e);
@@ -137,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
                             .setUserAuthenticationRequired(true)
                             .build());
             mKeyPairGenerator.generateKeyPair();
-        } catch (InvalidAlgorithmParameterException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
